e5dcd84190e13628ccb43a797444d94a132994cb
Edwin Puertas <eapuerta@gmail.com>
2022-10-26 08:31:59 -0500

    Add unittest

---
+++ logic/test.py
@@ -0,0 +1,24 @@
+import unittest
+from logic.student import Student
+
+
+class TestStudent(unittest.TestCase):
+    person = Student(id=1, name='Name', surname='Surname')
+
+    def test_instance(self):
+        self.assertIsInstance(self.person, Student, "Its instance!")
+
+    def test_id(self):
+        self.assertEqual(self.person.id, 1)
+
+    def test_name(self):
+        self.assertEqual(self.person.name, "Name")
+
+    def test_surname(self):
+        self.assertEqual(self.person.surname, 'Surname')
+
+    def test__str__(self):
+        self.assertEqual(self.person.__str__(), {'id': 1, 'name': 'Name', 'surname': 'Surname'})
+
+if __name__ == '__main__':
+    unittest.main()
