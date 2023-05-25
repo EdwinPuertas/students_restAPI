1219758833142c9f84c71c965d56da26b8782663
Edwin Puertas <eapuerta@gmail.com>
2023-05-24 21:04:53 -0500

    Update API v2

--- app.py
+++ app.py
@@ -1,21 +1,40 @@
+from fastapi import FastAPI, Form, Request, status
+from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import uvicorn
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
 from logic.student import Student
 from logic.student_controller import StudentController
 
 app = FastAPI()
 st_object = StudentController()
-origins = ["*"]
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    print('Request for index page received')
+    return templates.TemplateResponse('index.html', {"request": request})
+
+
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = 'favicon.ico'
+    file_path = './static/' + file_name
+    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
+
+
+@app.post('/hello', response_class=HTMLResponse)
+async def hello(request: Request, name: str = Form(...)):
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+
 
 @app.get("/")
 def read_root():
@@ -33,4 +52,4 @@ async def add(id: int, name: str, surname: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=33507)
+    uvicorn.run('app:app')
