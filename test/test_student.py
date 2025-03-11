9951a635223ea43bf454a80d266d36a14144988d
epuerta <eapuerta@gmail.com>
2025-03-11 17:51:14 -0500

    Update test

--- test/test_student.py
+++ test/test_student.py
@@ -7,9 +7,10 @@ class TestStudent(unittest.TestCase):
         self.student = Student(idn=123, name="Name", surname="SurName")
 
     def test_student(self):
-        self.assertEqual(self.student.idn, 123)
+        self.assertEqual(self.student.idn, "123")
         self.assertEqual(self.student.name, "Name")
         self.assertEqual(self.student.surname, "SurName")
+
 
 
 if __name__ == '__main__':
