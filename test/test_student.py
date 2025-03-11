3b1229bb92548f58679b4269d4029b954c1c75fd
epuerta <eapuerta@gmail.com>
2025-03-11 17:36:51 -0500

    Update test

--- test/test_student.py
+++ test/test_student.py
@@ -7,17 +7,17 @@ class TestStudent(unittest.TestCase):
         self.student = Student(idn=123, name="Name", surname="SurName")
 
     def test_student(self):
-        self.assertEqual(self.student.idn, "123")
+        self.assertEqual(self.student.idn, 123)
         self.assertEqual(self.student.name, "Name")
         self.assertEqual(self.student.surname, "SurName")
 
     def test_string_student(self):
-        with self.assertRaises(TypeError, msg="Student must be a string"):
+        with self.assertRaises(TypeError, msg="Student name and surname must be a string"):
             self.student.name = 'Name'
             self.student.surname = 'SurName'
 
     def test_int_student(self):
-        with self.assertRaises(TypeError, msg="Student must be a integer"):
-            self.student.idn = "123"
+        with self.assertRaises(TypeError, msg="Student idn must be a integer"):
+            self.student.idn = 123
 if __name__ == '__main__':
     unittest.main()
